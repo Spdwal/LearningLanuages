dd0234324caeb8666c07262d2b0e8c469591342f
spdwal <568741362@qq.com>
2019-02-14 17:27:16 +0800

    some code of a django blog

---
+++ Python/django/project1/blog/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+# Create your views here.
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post':post})
+
+#
+# def post_share(request, post_id):
+#     post = get_object_or_404(Post, id=post_id, status='published')
+#     sent = False
+#     if request.method == 'POST':
+#         form = EmailPostForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             post_url = request.build_absolute_uri(post.get_absolute_url())
+#             subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+#             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+#             send_mail(subject, message, '568741362@qq.com', [cd['to']])
+#             sent = True
+#     else:
+#         form = EmailPostForm()
+#
+#     return render(request, 'blog/post/share.html',
+#                   {'post': post,
+#                    'form': form})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
--- Python/django/project1/project1/urls.py
+++ Python/django/project1/project1/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""project1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from . import views
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('time/', views.current_datetime),
-    re_path(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
