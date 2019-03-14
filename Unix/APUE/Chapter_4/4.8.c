9536fb0efbbe400320f9476b8322b792a8772fe3
spdwal <568741362@qq.com>
2019-03-14 16:29:40 +0800

    complete APUE chapter 4

---
+++ Unix/APUE/Chapter_4/4.8.c
@@ -0,0 +1,20 @@
+#include <apue.h>
+
+
+int main(void){
+    char *ptr;
+    size_t size;
+
+    if(chdir("/usr/spool/uucppublic") < 0){
+        err_sys("chdir failed");
+    }
+
+    ptr = path_alloc(&size);
+    if(getcwd(ptr, size) == NULL){
+        err_sys("getcwd failed");
+    }
+
+    printf("cwd = %s\n", ptr);
+
+    exit(0);
+}
