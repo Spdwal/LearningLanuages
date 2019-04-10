b3455a0f5dccd91e57c01814d888903809aa980d
spdwal <568741362@qq.com>
2019-04-10 11:22:38 +0800

    a client which takes 5 connects to server

---
+++ Unix/UNP/Vol1/Chapter_5/5.7.c
@@ -0,0 +1,22 @@
+#include <unp/unp.h>
+
+int main(int argc, char **argv){
+    int i, sockfd[5];
+
+    struct sockaddr_in servaddr;
+
+    for(i = 0; i < 5; i++){
+        sockfd[i] = Socket(AF_INET, SOCK_STREAM, 0);
+
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(SERV_PORT);
+        Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+        Connect(sockfd[i], (SA *) &servaddr, sizeof(servaddr));
+    }
+
+    str_cli(stdin, sockfd[0]);
+
+    exit(0);
+}
